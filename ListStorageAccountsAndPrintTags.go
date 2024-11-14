bc6e02a483af6ad05e6737a6b951686fda2d385d
Brant Pastore <brant.pastore@gmail.com>
2024-11-13 18:23:52 -0700

    Separated code into function GetResourceTags

--- ListStorageAccountsAndPrintTags.go
+++ ListStorageAccountsAndPrintTags.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 
+	// "github.com/Azure/azure-sdk-for-go/sdk/azcore/internal/exported"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/subscription/armsubscription"
@@ -52,19 +54,31 @@ func main() {
 				resourceID := *rg.ID
 				resources[resourceID]++
 				fmt.Printf("\r\nStorageAccount. ID: %s, ResourceGroup: %s\r\n", *rg.ID, *rg.Name)
-
-				tags, err := armresources.NewTagsClient(subscriptionID, cred, nil)
-				if err != nil {
-					fmt.Errorf("Error creating tags client: %s", err)
-				}
-				resourceTags, err := tags.GetAtScope(context.TODO(), resourceID, nil)
-				if err != nil {
-					fmt.Errorf("Error getting tags: %s", err)
-				}
-				for key, value := range resourceTags.Properties.Tags {
-					fmt.Println("[Tags] Key:", key, "Value:", *value)
-				}
+				GetResourceTags(subscriptionID, resourceID, cred)
 			}
 		}
+	}
+}
+
+func GetResourceTags(subscriptionId string, resourceId string, credential azcore.TokenCredential) map[string]string {
+	kvMap := make(map[string]string)
+	tags, err := armresources.NewTagsClient(subscriptionID, credential, nil)
+	if err != nil {
+		fmt.Errorf("Error creating tags client: %s", err)
+	}
+	resourceTags, err := tags.GetAtScope(context.TODO(), resourceId, nil)
+	if err != nil {
+		fmt.Errorf("Error getting tags: %s", err)
+	}
+	for key, value := range resourceTags.Properties.Tags {
+		fmt.Println("[Tags] Key:", key, "Value:", *value)
+		kvMap[key] = *value
+	}
+
+	if kvMap != nil {
+		return kvMap
+	} else {
+		fmt.Errorf("Map is nil")
+		return nil
 	}
 }
